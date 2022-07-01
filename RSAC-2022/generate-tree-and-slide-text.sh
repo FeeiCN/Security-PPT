fe6bdf4e0fe7c2b673936ecaa883905d5e11fc6f
Feei <feei@feei.cn>
2022-07-01 22:26:32 +0800

    add some security papers

---
+++ RSAC-2022/generate-tree-and-slide-text.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Generate tree.txt and each slide PDF's text
+# Usage  : ./generate-tree-and-slide-text.sh
+# Author : Feei <feei#feei.cn>
+# Updated: 15/Jun/2022
+
+# Generate tree
+tree > tree.txt
+
+# Generate each slide PDF to HTML
+if ! command -v pdftotext &> /dev/null
+then
+  echo "xpdf tool could not be found, you can use \`brew install xpdf\`!"
+  exit
+fi
+
+find . -type f -name '*.pdf' -print0 | while IFS= read -r -d '' file; do
+  echo "Generate $file to $file.html"
+  pdftotext "$file" "$file.html"
+done
